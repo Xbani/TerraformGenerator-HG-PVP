8df1009ee634a8ecff7df62113dcc3072491d039
Oscar Bonduelle <oscar.bonduelle@hotmail.fr>
2026-07-05 13:38:31 +0200

    Add vanilla temple structures and summon command

--- common/src/main/java/org/terraform/main/TerraformCommandManager.java
+++ common/src/main/java/org/terraform/main/TerraformCommandManager.java
@@ -90,6 +90,7 @@ public class TerraformCommandManager implements TabExecutor {
 
         }
         this.registerCommand(new LocateCommand(plugin, "locate"));
+        this.registerCommand(new SummonStructureCommand(plugin, "summon"));
         this.registerCommand(new FixerCacheFlushCommand(plugin, "fixercacheflush", "fcf"));
         this.registerCommand(new LocateBiomeCommand(plugin, "locatebiome", "lb"));
         this.registerCommand(new DebugCachePushCommand(plugin, "debugcachepush"));
